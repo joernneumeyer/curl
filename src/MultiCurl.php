255f7055e97f32807c8ef14eeec5cd198104b04b
Jörn Neumeyer <contact@joern-neumeyer.de>
2021-10-13 15:51:21 +0200

    add doc comments

--- src/MultiCurl.php
+++ src/MultiCurl.php
@@ -4,16 +4,31 @@
 
   namespace Neu\Curl;
 
+  /**
+   * @package Neu\Curl
+   */
   class MultiCurl {
+    /**
+     * @var \CurlMultiHandle|resource
+     */
     private $handle;
+    /**
+     * @var array<Curl>
+     */
     private $subHandles = [];
 
     public const PARSE_BODY = 1;
 
+    /**
+     * @param null|resource|\CurlMultiHandle $handle
+     */
     public function __construct($handle = null) {
       $this->handle = $handle ?? curl_multi_init();
     }
 
+    /**
+     * @return \CurlMultiHandle|resource
+     */
     public function handle() {
       return $this->handle;
     }
@@ -22,30 +37,58 @@
       $this->close();
     }
 
+    /**
+     * @link curl_multi_close()
+     */
     public function close(): void {
       curl_multi_close($this->handle);
     }
 
+    /**
+     * @link curl_multi_setopt()
+     * @param int $opt
+     * @param mixed $value
+     * @return bool
+     */
     public function setOpt(int $opt, $value): bool {
       return curl_multi_setopt($this->handle, $opt, $value);
     }
 
+    /**
+     * @link curl_multi_errno()
+     * @return int
+     */
     public function errno(): int {
       return curl_multi_errno($this->handle);
     }
 
-    public function content(): ?string {
-      return curl_multi_getcontent($this->handle);
-    }
-
+    /**
+     * @link curl_multi_strerror()
+     * @return string|null
+     */
     public function strError(): ?string {
       return curl_multi_strerror($this->errno());
     }
 
+    /**
+     * @link curl_multi_select()
+     * @param float $timeout
+     * @return int
+     */
     public function select(float $timeout = 1.0): int {
       return curl_multi_select($this->handle, $timeout);
     }
 
+    /**
+     * Executes the underlying cURL multi handle.
+     *
+     * All sub-handles are added to and removed from the multi-handle inside this method.
+     * if the $export parameter is set to @link MultiCurl::PARSE_BODY, all incoming JSON responses
+     * will be parsed and then added to the result set.
+     * All other responses are added as strings.
+     * @param int $export
+     * @return array<mixed>
+     */
     public function exec(int $export = 0): array {
       foreach ($this->subHandles as $sh) {
         curl_multi_add_handle($this->handle, $sh->handle());
@@ -75,6 +118,7 @@
     }
 
     /**
+     * Add a sub-handle for later execution.
      * @param Curl $handle
      * @return void
      */
@@ -83,6 +127,7 @@
     }
 
     /**
+     * Remove a sub-handle.
      * @param Curl $handle
      * @return void
      */
